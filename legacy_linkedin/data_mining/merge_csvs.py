7d0e6ca19392e7fcf7ea1048c29b352926d9f70f
Sergii Volodko <volodkosm@gmail.com>
2026-02-21 14:26:35 +0000

    move to legacy_linkedin folder

--- legacy_linkedin/data_mining/merge_csvs.py
+++ legacy_linkedin/data_mining/merge_csvs.py
@@ -3,6 +3,8 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from company_miner.common.CsvRepository import CsvRepository
 
+# Moved data files to Dropbox
+
 if __name__ == "__main__":
     print("Script started")
     all_companies = []
